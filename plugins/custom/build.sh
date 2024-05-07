bebf91e903a414a520954e6cfe6a93b019caeb09
Walter Van Herck <Walter.Vanherck@iter.org>
2024-05-07 09:21:11 +0000

    Add var attributes into examples

--- plugins/custom/build.sh
+++ plugins/custom/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 g++ -c -Wall -Werror -fpic -I /opt/codac/include fail_instruction.cpp
-g++ -shared -o libfail.so fail_instruction.o
+g++ -shared -o libfail.so -L/opt/codac/lib -lsequencer fail_instruction.o
